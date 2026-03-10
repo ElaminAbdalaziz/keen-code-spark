90854a579920b5d42b89632b4eb438c9c85febd5
Abdalaziz Elamin <131213410+ElaminAbdalaziz@users.noreply.github.com>
2026-03-10 06:51:10 +0200

    Wrap App component in React.StrictMode and BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode Basenme = {import.meta.env.BASE_URL}>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "*" element = {<App/>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
